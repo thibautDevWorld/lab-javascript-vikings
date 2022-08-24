b11388da4aa0e66f7c9f454df082e80318b9b16a
Thibaut Lartigue <thibaut.lartigue.pro@gmail.com>
2022-08-24 12:38:17 +0200

    iteration 2 done

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,33 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+        
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else if(!this.health) {
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+
+
+}
+
+let newViking = new Viking('Odin', 50, 50);
+
+newViking.receiveDamage(20)
 
 // Saxon
 class Saxon {}
